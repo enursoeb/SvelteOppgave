ea7eb169988e3c8dccddeb6286a9aec2ec2573f5
Rune Søbye <rune@flytit.no>
2021-11-01 08:58:47 +0100

    cleanup leftovers and comments

--- src/Components/MovieDetails.svelte
+++ src/Components/MovieDetails.svelte
@@ -5,7 +5,7 @@
   import { createEventDispatcher } from "svelte";
   import Modal from "../Shared/Modal.svelte";
   import AddRackForm from "./AddRackForm.svelte";
-  import {get} from 'svelte/store';
+  import { get } from "svelte/store";
 
   export let movie;
   export let isSearch;
@@ -13,84 +13,90 @@
   let showModal = false;
   let dispatch = createEventDispatcher();
 
-  // Deleting movie
+  // Remove movie from collection
   const handleDelete = (id) => {
     MovieStore.update((currentMovies) => {
-      return currentMovies.filter( (cm) => cm.Id != id);
+      return currentMovies.filter((cm) => cm.Id != id);
     });
   };
 
-  // Move to collection
+  // Add movie to collection
   const addToCollection = (id) => {
-    
-     dispatch("removeFromSearch", movie);
+    dispatch("removeFromSearch", movie);
 
-        MovieStore.update(currentMovies => {
-          movie.Id = movie.imdbID;
-          return [movie, ...currentMovies];
-        });
+    MovieStore.update((currentMovies) => {
+      movie.Id = movie.imdbID;
+      return [movie, ...currentMovies];
+    });
   };
 
+  // Remove movie from search results
   const removeSearch = (id) => {
     dispatch("removeFromSearch", movie);
   };
 
   // Open/close modal
-const toggleModal = () => {
-  showModal = !showModal
-};
+  const toggleModal = () => {
+    showModal = !showModal;
+  };
 
-const addRack = (e) => {
-  movie.Rack = e.detail;
-  showModal = false;
+  // Add rack for provided movie
+  const addRack = (e) => {
+    movie.Rack = e.detail;
+    showModal = false;
 
-  if(isSearch) {
-    addToCollection(movie);
-  }
-}
+    if (isSearch) {
+      addToCollection(movie);
+    }
+  };
 </script>
 
 <div class="movie-results">
-<Card>
-  Tittel: {movie.Title} <br />
-  År: {movie.Year} <br />
-  Sjanger: {movie.Genre} <br>
-  Hylle: {movie.Rack}  <br>
-  <div> <img class="image" src={movie.Poster} alt="{movie.Tittel}"/></div>
-   
+  <Card>
+    Tittel: {movie.Title} <br />
+    År: {movie.Year} <br />
+    Sjanger: {movie.Genre} <br />
+    Hylle: {movie.Rack} <br />
+    <div><img class="image" src={movie.Poster} alt={movie.Tittel} /></div>
+
     <div class="delete">
       {#if !isSearch}
-      <Button flat={true} on:click={ () => handleDelete(movie.Id)}>Slett</Button> 
-      <Modal {showModal} on:click={toggleModal}>
-        <AddRackForm on:addRack={addRack}/>
-      </Modal>   
-      <Button flat={true} type="secondary" on:click={toggleModal}>Endre hylle</Button>
+        <Button flat={true} on:click={() => handleDelete(movie.Id)}
+          >Slett</Button
+        >
+        <Modal {showModal} on:click={toggleModal}>
+          <AddRackForm on:addRack={addRack} />
+        </Modal>
+        <Button flat={true} type="secondary" on:click={toggleModal}
+          >Endre hylle</Button
+        >
       {/if}
       {#if isSearch && disableButton === false}
-      <Modal {showModal} on:click={toggleModal}>
-      <AddRackForm on:addRack={addRack}/>
-    </Modal>   
-      <Button flat={true} type="secondary" on:click={toggleModal}>Flytt til samling</Button> 
+        <Modal {showModal} on:click={toggleModal}>
+          <AddRackForm on:addRack={addRack} />
+        </Modal>
+        <Button flat={true} type="secondary" on:click={toggleModal}
+          >Flytt til samling</Button
+        >
       {/if}
       {#if isSearch}
-      <Button flat={true} on:click={removeSearch}>Fjern fra søk</Button> 
+        <Button flat={true} on:click={removeSearch}>Fjern fra søk</Button>
       {/if}
     </div>
-</Card>
+  </Card>
 </div>
 
 <style>
-.image {
- margin-top: 12px;
- max-width: 480px;
- max-height: 240px;
-}
-    .movie-results {
-        display: grid;
-        grid-template-columns: auto auto auto;
-        grid-template-rows: auto auto auto;
-        column-gap: 10px;
-        row-gap: 15px;
-    }
+  .image {
+    margin-top: 12px;
+    max-width: 480px;
+    max-height: 240px;
+  }
+  .movie-results {
+    display: grid;
+    grid-template-columns: auto auto auto;
+    grid-template-rows: auto auto auto;
+    column-gap: 10px;
+    row-gap: 15px;
+  }
 </style>
-
--- src/Components/MovieSearch.svelte
+++ src/Components/MovieSearch.svelte
@@ -2,32 +2,36 @@
     import MovieDetails from "./MovieDetails.svelte";
     import { flip } from "svelte/animate";
     import { fade, slide, scale } from "svelte/transition";
-    import MovieStore from "../Stores/MovieStore"; 
-    import {get} from 'svelte/store';
+    import MovieStore from "../Stores/MovieStore";
+    import { get } from "svelte/store";
 
     let search = "";
     let isSearch = true;
     let searchResult = [];
-    
+
+    // Get search result
     const handleSubmit = async () => {
         var apiUrl = `http://www.omdbapi.com/?t=${search}&apikey=311042d1`;
         var res = await fetch(apiUrl)
             .then((response) => response.json())
             .then((data) => {
-                if(data.Response == "True") {
-                    if(data.Poster == "N/A") {
+                if (data.Response == "True") {
+                    if (data.Poster == "N/A") {
                         data.Poster = "Images/NoImage.jpg";
                     }
-                searchResult = [...searchResult, data];
+                    searchResult = [...searchResult, data];
                 }
             });
     };
 
+    // Remove provided movie from search result
     const removeFromSearch = (movieToBeRemoved) => {
-        searchResult = searchResult.filter((sr) => sr.imdbID != movieToBeRemoved.detail.imdbID);
+        searchResult = searchResult.filter(
+            (sr) => sr.imdbID != movieToBeRemoved.detail.imdbID
+        );
     };
 
-   let movieCollection = get(MovieStore);
+    let movieCollection = get(MovieStore);
 </script>
 
 <!-- Search -->
@@ -38,23 +42,25 @@
 
 <!-- display search results -->
 <div class="movie-list">
-{#each searchResult as movie}
+    {#each searchResult as movie}
         <MovieDetails
             {movie}
             {isSearch}
-            disableButton =  {movieCollection.find(mc => mc.Id == movie.imdbID) != undefined}
+            disableButton={movieCollection.find(
+                (mc) => mc.Id == movie.imdbID
+            ) != undefined}
             on:removeFromSearch={removeFromSearch}
         />
-{/each}
+    {/each}
 </div>
 
 <style>
-.movie-list {
-    display: grid;
-    grid-template-columns: auto auto auto;
-    grid-template-rows: auto auto auto;
-    column-gap: 10px;
-    align-content: space-around;
-    row-gap: 15px;
-}
+    .movie-list {
+        display: grid;
+        grid-template-columns: auto auto auto;
+        grid-template-rows: auto auto auto;
+        column-gap: 10px;
+        align-content: space-around;
+        row-gap: 15px;
+    }
 </style>
