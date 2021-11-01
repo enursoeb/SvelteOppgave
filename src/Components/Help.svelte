830295b0e8c77ba1da71f33e9f5061ddee53ecf3
Rune Søbye <rune@flytit.no>
2021-11-01 13:27:12 +0100

    wip: help page

--- src/Components/Help.svelte
+++ src/Components/Help.svelte
@@ -1,28 +1,61 @@
 <script>
-
 </script>
 
-<div>Siden består av to hovedelementer: <b>Filmoversikt</b> og <b>Finn filmer</b></div> <br>
-
+<div>
+    Siden består av to hovedelementer: <b>Filmoversikt</b> og <b>Finn filmer</b>
+</div>
 
 <h3>Filmoversikt</h3>
-    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin convallis, nisi eget lobortis posuere, <br>
-    libero libero pulvinar sapien, ac fermentum dui nisi nec erat. Vivamus hendrerit sodales laoreet. <br>
-    Nunc gravida in turpis sit amet faucibus. Aliquam bibendum feugiat tellus, a suscipit leo blandit a <br>. Donec eget magna nec eros ornare ultricies vel sit amet lorem. <br>
-    Maecenas scelerisque ligula ut ligula tempor, vel varius diam elementum. Sed ut velit odio.
+<div>
+    Filmer kan filtreres etter hvilke hyller man har registrert de på. <br />
+    Dette gjøres ved å velge hylle i nedtrekksmenyen under 'Filtrer samling etter
+    hylle'.
+</div>
+<img class="image" src="Images/DropDown.png" alt="velg hylle" />
 
-Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. <br> Nunc accumsan egestas risus nec gravida. Pellentesque convallis tellus purus, vel convallis magna iaculis eu. <br>
- Vivamus iaculis sem in quam mattis malesuada. Vestibulum euismod congue mauris. Mauris eu eros suscipit, interdum orci quis, egestas erat. Proin non varius elit.
+<div>
+    Hvis du ønsker å ikke filtrere på hylle, velg <b>Alle</b> i nedtrekkslista.
+</div>
+<img class="image" src="Images/DropDownAll.png" alt="vis alle filmer" />
 
+<div>
+    Etter at man har valgt foretrukket visningsmetode, vil det for hver film i <br>
+    samlingen vises  informasjon og bilde av filmplakaten. <br /> For hver film kan man velge å
+    knytte filmen til ny hylle ved å klikke <edit class="edit">endre hylle-knappen </edit> <br>
+    eller fjerne filmen ved å klikke <delete class="deleteMovie">slett-knappen.</delete>
+</div>
+<img class="image" src="Images/MovieOptions.png" alt="movieoptions" />
 
+<p />
+<h3>Finn filmer</h3>
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin convallis, nisi eget
+lobortis posuere,<br />
+libero libero pulvinar sapien, ac fermentum dui nisi nec erat. Vivamus hendrerit
+sodales laoreet. <br />
+Nunc gravida in turpis sit amet faucibus. Aliquam bibendum feugiat tellus, a suscipit
+leo blandit a <br />. Donec eget magna nec eros ornare ultricies vel sit amet
+lorem. <br />
+Maecenas scelerisque ligula ut ligula tempor, vel varius diam elementum. Sed ut velit
+odio. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos
+himenaeos. <br /> Nunc accumsan egestas risus nec gravida. Pellentesque
+convallis tellus purus, vel convallis magna iaculis eu. <br />
+Vivamus iaculis sem in quam mattis malesuada. Vestibulum euismod congue mauris. Mauris
+eu eros suscipit, interdum orci quis, egestas erat. Proin non varius elit.
 
-<p>
-<h3> Finn filmer</h3>
-Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin convallis, nisi eget lobortis posuere, <br>
-    libero libero pulvinar sapien, ac fermentum dui nisi nec erat. Vivamus hendrerit sodales laoreet. <br>
-    Nunc gravida in turpis sit amet faucibus. Aliquam bibendum feugiat tellus, a suscipit leo blandit a <br>. Donec eget magna nec eros ornare ultricies vel sit amet lorem. <br>
-    Maecenas scelerisque ligula ut ligula tempor, vel varius diam elementum. Sed ut velit odio.
+<style>
+    .image {
+        display: inline-block;
+        align-content: center;
+        border: 5px solid #555;
+        margin-top: 12px;
+        margin-bottom: 12px;
+    }
 
-Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. <br> Nunc accumsan egestas risus nec gravida. Pellentesque convallis tellus purus, vel convallis magna iaculis eu. <br>
- Vivamus iaculis sem in quam mattis malesuada. Vestibulum euismod congue mauris. Mauris eu eros suscipit, interdum orci quis, egestas erat. Proin non varius elit.
-<style></style>
+    .edit {
+        color: green;
+    }
+
+    .deleteMovie {
+        color: red;
+    }
+</style>
