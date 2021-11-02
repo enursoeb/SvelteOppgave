776466a75b017865fb3e0ac2f61bdce331c331aa
Rune Søbye <rune@flytit.no>
2021-11-02 14:34:34 +0100

    some changes on help page

--- src/Components/Help.svelte
+++ src/Components/Help.svelte
@@ -2,13 +2,14 @@
 </script>
 
 <div>
-    Siden består av to hovedelementer: <b>Filmoversikt</b> og <a href="#moviesearch"><b>Finn filmer</b> </a>
+    Siden består av to hovedelementer: <b>Filmoversikt</b> og
+    <a href="#moviesearch"><b>Finn filmer</b> </a>
 </div>
 
 <h3>Filmoversikt</h3>
-<div>
-    Filmer kan filtreres etter hvilke hyller man har registrert de på. <br />
-    Dette gjøres ved å velge hylle i nedtrekksmenyen under 'Filtrer samling etter
+<div class="text">
+    Filmer kan filtreres etter hvilke hyller man har registrert de på. <br>Dette gjøres ved å 
+    velge hylle i nedtrekksmenyen under 'Filtrer samling etter
     hylle'.
 </div>
 <img class="image" src="Images/DropDown.png" alt="velg hylle" />
@@ -18,47 +19,53 @@
 </div>
 <img class="image" src="Images/DropDownAll.png" alt="vis alle filmer" />
 
-<div>
-    Etter at man har valgt foretrukket visningsmetode, vil det for hver film i <br>
-    samlingen vises  informasjon og bilde av filmplakaten. <br /> For hver film kan man velge å
-    knytte filmen til ny hylle ved å klikke <edit class="edit">endre hylle-knappen </edit> <br>
-    eller fjerne filmen ved å klikke <delete class="deleteMovie">slett-knappen.</delete>
+<div class="text">
+    Etter at man har valgt foretrukket visningsmetode, vil det for hver film i <br
+    />
+    samlingen vises informasjon og bilde av filmplakaten. <br /> For hver film
+    kan man velge å knytte filmen til ny hylle ved å klikke
+    <edit class="edit">endre hylle-knappen </edit> <br />
+    eller fjerne filmen ved å klikke
+    <delete class="deleteMovie">slett-knappen.</delete> <br>
 
-    Ved klikk på <b>endre hylle</b>, vil det dukke opp en dialogboks. Se <a href="#modal">her </a> for instruksjoner om hvordan
-    den virker.
-
-
+    Ved klikk på <b>endre hylle</b>, vil det dukke opp en dialogboks. Se
+    <a href="#modal">her </a> for instruksjoner om hvordan den virker.
 </div>
 <img class="image" src="Images/MovieOptions.png" alt="movieoptions" />
 
 <a name="moviesearch"><h3 id="findmovies">Finn filmer</h3></a>
-<div>
-Dette er siden hvor man kan legge til nye filmer i sin samling. <br>
-Plasser markøren i søkefeltet og skriv inn tittel på film du ønsker å finne <br> 
-(det er foreløpig ikke støtte for avansert søk.) <br>Etter at man har søkt ved å klikk på søk-knappen, 
-vil det vises filminfo for alle treff, <br> tilsvarende oversikt-siden.
-Klikk <edit class="edit"> grønn knapp </edit> for å flytte en film til samlingen, 
-eller <br> <delete class="deleteMovie">rød knapp</delete> for å fjerne film fra søkeresultater. <br>
+<div class="text">
+    Dette er siden hvor man kan legge til nye filmer i sin samling. <br />
+    Plasser markøren i søkefeltet og skriv inn tittel på film du ønsker å finne
+    <br />
+    (det er foreløpig ikke støtte for avansert søk.) <br />Etter at man har søkt
+    ved å klikk på søk-knappen, vil det vises filminfo for alle treff, <br />
+    tilsvarende oversikt-siden. Klikk <edit class="edit"> grønn knapp </edit> <br>
+    for å flytte en film til samlingen, eller <br />
+    <delete class="deleteMovie">rød knapp</delete>
+    for å fjerne film fra søkeresultater. <br />
 </div>
-
 
 <img class="image" src="Images/MovieSearch.png" alt="moviesearch" />
 
-<div>
-<a name="modal">
-<h4>Endre hylle / Flytt til samling</h4>
-</a>
-Når man velger å flytte film til søk fra samling, eller endre hylle på søkesiden, <br>
-vil det dukke opp et nytt vindu hvor man bes om informasjon om hylla til filmen. <br>
-Her kan man velge å enten <b>opprette ny hylle </b>, eller <b> <br>velge  fra allerede eksisterende hyller</b>,
-dette velges ved å klikke på boksen til høyre for ønsket valg.
-Angi ditt valg med å skrive inn navn i tekstboksen eller klikk ønsket hylle fra rullegardinmenyen, og klikk deretter på
-lagre endringer. Hvis du angrer og ikke ønsker å lagre, klikk på avbryt-knappen.
-
+<div class="text">
+    <a name="modal">
+        <h4>Endre hylle / Flytt til samling</h4>
+    </a>
+    Når man velger å flytte film til søk fra samling, eller endre hylle på søkesiden,
+    <br />
+    vil det dukke opp et nytt vindu hvor man bes om informasjon om hylla til filmen.
+    <br />
+    Her kan man velge å enten <b>opprette ny hylle </b>, eller
+    <b> <br />velge fra allerede eksisterende hyller</b>, dette velges ved å
+    klikke på boksen til høyre for ønsket valg. <br>Angi ditt valg med å skrive inn
+    navn i tekstboksen eller klikk ønsket hylle fra rullegardinmenyen, <br>og klikk
+    deretter på lagre endringer. Hvis du angrer og ikke ønsker å lagre, klikk på
+    avbryt-knappen.
 </div>
 
-<img class="image" src="Images/ModalTextInput.png" alt="modaltext"/>
-<img class="image" src="Images/ModalSelect.png" alt="modalselect"/>
+<img class="image" src="Images/ModalTextInput.png" alt="modaltext" />
+<img class="image" src="Images/ModalSelect.png" alt="modalselect" />
 
 <style>
     .image {
@@ -69,8 +76,8 @@ lagre endringer. Hvis du angrer og ikke ønsker å lagre, klikk på avbryt-knapp
         margin-bottom: 12px;
         margin-right: 12px;
         margin-left: 12px;
-        max-width: 360;
-        max-height: 245;
+        width: 420px;
+        height: 275px;
     }
 
     .edit {
@@ -79,5 +86,9 @@ lagre endringer. Hvis du angrer og ikke ønsker å lagre, klikk på avbryt-knapp
 
     .deleteMovie {
         color: red;
+    }
+
+    .text {
+        text-align: center;
     }
 </style>
